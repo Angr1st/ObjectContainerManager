312eb9b848c030beeef458ba22a36ee88f9cef90
Torben Clasen <torbenclasen@gmail.com>
2018-03-29 19:11:52 +0200

    Finished upgrading to dotnetstandard2.0

--- src/ObjectContainerManager/Properties/AssemblyInfo.cs
+++ src/ObjectContainerManager/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ObjectContainerManager")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Torben Clasen 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
